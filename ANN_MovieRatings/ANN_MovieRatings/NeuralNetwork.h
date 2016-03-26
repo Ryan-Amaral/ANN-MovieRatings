4b1a5b7d6c4a462b8505b9b4091b58f9facc3bc2
ryan-amaral <ryan_amaral@live.com>
2016-03-26 13:29:14 -0300

    Added classes

---
+++ ANN_MovieRatings/ANN_MovieRatings/NeuralNetwork.h
@@ -0,0 +1,19 @@
+//  File: NeuralNetwork.h
+//	Purpose: Header file for NeuralNetwork.cpp
+//  Project: ANN_MovieRatings
+//  Author: Ryan Amaral
+//  Created On: March 26, 2016
+
+#ifndef _NEURAL_NETWORK_H
+#define _NEURAL_NETWORK_H
+
+class NeuralNetwork
+{
+private:
+	int _layers; // how many layers are in the net
+	int* _layerSizes; // the size of each layer
+	int** _neurons;
+public:
+};
+
+#endif
