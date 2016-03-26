91b9ed9014e739d46210ecf3380bf01894eb92ce
ryan-amaral <ryan_amaral@live.com>
2016-03-26 13:44:30 -0300

    Added content to neural network header

--- ANN_MovieRatings/ANN_MovieRatings/NeuralNetwork.h
+++ ANN_MovieRatings/ANN_MovieRatings/NeuralNetwork.h
@@ -7,13 +7,22 @@
 #ifndef _NEURAL_NETWORK_H
 #define _NEURAL_NETWORK_H
 
+/// Standard feedforward neural network. Topology can be created on instantiation.
+/// All non-input neurons have biases, and all neurons are clamped between 0 and 1.
+/// Weights start between -1 and 1, but can grow to be any value as the GA sees fit.
 class NeuralNetwork
 {
 private:
 	int _layers; // how many layers are in the net
 	int* _layerSizes; // the size of each layer
-	int** _neurons;
+	float** _neuronValues; // the neurons (values of), first index is layer, second is neuron in layer
+	float*** _weights; // the weights connecting neurons together
+					   // first index is layer, second is start neuron, third is end neuron
+	float** _biases; // the bias for each neuron
 public:
+	NeuralNetwork(int layers, int* topology);
+	void feedForward(float* values);
+	float getValue() const;
 };
 
 #endif
